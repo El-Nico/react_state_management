27b40fbf34422b24abcaf80229f6b2415946723a
El-Nico <nicholasc1665@yahoo.com>
2023-09-21 15:25:59 +0100

    completed usememo and usecallback

--- native-useMemoAndCallback/src/App.jsx
+++ native-useMemoAndCallback/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useMemo, useCallback } from "react";
+
+// use usememo whenever you do a complex calculation that you dont
+// want to recalculate on every render
+// basically track a state and only rerender on state change??
+// only use for arrays or objects
+
+// do not use when calculation is simple and results in a number
+// or scalar, only use when calculation is complex and results
+// in big array or object
+
+function SortedList({ list, sortFunc }) {
+  console.log("SortedList render");
+
+  const sortedList = useMemo(() => {
+    console.log("Running sort");
+    return [...list].sort(sortFunc);
+  }, [list, sortFunc]);
+
+  return <div>{sortedList.join(", ")}</div>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numbers] = useState([10, 20, 30]);
+
+  const total = useMemo(
+    () => numbers.reduce((acc, number) => acc + number, 0),
+    [numbers]
+  );
+
+  const [names] = useState(["John", "Paul", "George", "Ringo"]);
+
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const countTotal = count1 + count2;
+
+  // usecallback memorizes the sort function so it doenst
+  // trigger use memo on every rerencer
+  // will only trigger if sort func changes
+
+  //use this to stabilize references u pass to a reacomponet
+  //also use when creating a custom hook make sure the reference
+  //to cb function is stable
+  const sortFunc = useCallback((a, b) => a.localeCompare(b) * -1, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Total: {total}</div>
+      <div>Names: {names.join(", ")}</div>
+      <SortedList list={names} sortFunc={sortFunc} />
+      <button onClick={() => setCount1(count1 + 1)}>Count1: {count1}</button>
+      <button onClick={() => setCount2(count2 + 1)}>Count2: {count2}</button>
+      <div>Total: {countTotal}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
