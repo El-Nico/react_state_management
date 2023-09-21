04d1a4b47de5b645f1c2d5cc494d7360abe6627b
El-Nico <nicholasc1665@yahoo.com>
2023-09-21 16:39:09 +0100

    completed useRef

--- native-useRef/src/App.jsx
+++ native-useRef/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect, useState } from "react";
 
+//useref hook can set state without triggering a rerender
+//inpractice it is used to get references to html element
+//or provide an initial value...
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef(null);
+
+  // use effect also produces the same effect as afterviewinit
+  // runs on render complet
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  //in this case usefref can be used to track nameid
+  //but usestate is not appropriate because no need to
+  //rerender on idincremented
+  const idRef = useRef(1);
+  const [names, setNames] = useState([
+    { id: idRef.current++, name: "John" },
+    { id: idRef.current++, name: "Jane" },
+  ]);
+
+  //can use input ref to get current name from input element
+  //inplace of a onchange handler
+  const onAddName = () => {
+    setNames([
+      ...names,
+      {
+        id: idRef.current++,
+        name: inputRef.current.value,
+      },
+    ]);
+    inputRef.current.value = "";
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {names.map((name) => (
+          <div key={name.name}>
+            {name.id} - {name.name}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <input type="text" ref={inputRef} />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
